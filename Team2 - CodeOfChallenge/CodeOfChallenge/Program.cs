666f084d9b8df594c8123e0d0fb60eecab49857f
asunger12 <apoorvsunger12@gmail.com>
2026-08-21 12:17:29 +0530

    fix(BUG-101): CodeOfChallenge/CodeOfChallenge/Program.cs, Sum(int a, int b), line 10: returns a * b instead of a + b
    Session: 4d9e7a32-c495-4d71-a297-d80b42b92372

---
+++ Team2 - CodeOfChallenge/CodeOfChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿class TestProgram
+{
+    public static void Main()
+    {
+        int result = Sum(1, 2);
+        Console.WriteLine(result);
+    }
+    static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
